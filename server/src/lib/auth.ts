1ab8ce2c7af76c4c9474303260400ebd3bdc7298
aaksant <aaksangit@gmail.com>
2026-01-09 22:23:02 +0700

    feat: pass and map remaining tables

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,4 +1,5 @@
 import { betterAuth } from "better-auth";
+import { openAPI } from "better-auth/plugins";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
 import * as schema from "../db/schema";
@@ -7,11 +8,14 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema: {
-      ...schema
-      // TODO: add when schema is fixed
-      // user: schema.users
+      ...schema,
+      user: schema.users,
+      account: schema.accounts,
+      session: schema.sessions,
+      verification: schema.verifications
     }
   }),
+  plugins: [openAPI()],
   emailAndPassword: {
     enabled: true
   },
